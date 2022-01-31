65331b45291b24d8bf3791e9d48a314e6002bc45
Pietro Boccaletto <pboccaletto@gmail.com>
2022-01-31 10:12:42 +0100

    some tweaking for the network module

--- libraries/network.py
+++ libraries/network.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import pandas as pd
+import ray
+ray.init()
+from os import path, listdir
+import modin.pandas as pd
+#import pandas as pd
 from sklearn import preprocessing
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -10,7 +14,6 @@ import sys
 from math import exp
 import pickle as pk
 import json
-from os import path, listdir
 from sys import argv
 from filterbyevalue import filterevalue
 
@@ -69,8 +72,11 @@ def loadData(inputf):
     else:
         print(f"File {inputf} not valid")
         exit()
-    df.columns = ["node1","qstart","qend","qlen","qseq","node2","eval","pident","bitscore","sstart","send","slen","length","sseq"]
-    df = df.drop(columns=["qend","qstart","qlen","qseq","pident","bitscore","sstart","send","slen","length","sseq"])
+    if len(df.columns) == 14:
+        df.columns = ["node1","qstart","qend","qlen","qseq","node2","eval","pident","bitscore","sstart","send","slen","length","sseq"]
+        df = df.drop(columns=["qend","qstart","qlen","qseq","pident","bitscore","sstart","send","slen","length","sseq"])
+    else:
+        df.columns = ["node1","node2","eval"]
     print("CSV loaded into Pandas Dataframe - lenght of dataset : {}".format(str(len(df))))
     return df
 
@@ -125,8 +131,8 @@ def mergeHits(df):
     print("Merging values for same node1 and node2 - start")
     print(df)
     aggregation_functions = {'eval': 'median',}
-    df_new = df.groupby(df['node1','node2']).aggregate(aggregation_functions)
-    print("len of the dataframe after cleaning: " + str(len(df)))
+    df_new = df.groupby(df['node1', 'node2']).aggregate(aggregation_functions)
+    print("len of the dataframe after merging: " + str(len(df)))
     print("Merging - Finish")
     return(df_new)
 
@@ -155,20 +161,41 @@ def networkxLoad(df):
         node1 = df.iloc[i]['node1']
         node2 = df.iloc[i]['node2']
         eval = df.iloc[i]['eval']
+        if eval == 0:
+            eval = 0.001 # Avoid removal of edges that had the lowest evalue
         if node1 not in nodeList:
-          nodeList.append(node1)
+            nodeList.append(node1)
         if node2 not in nodeList:
-          nodeList.append(node2)
-        edgeList.append((node1,node2,eval))
+            nodeList.append(node2)
+        edgeList.append((node1, node2, eval))
     for n in nodeList:
-          G.add_node(n)
+        G.add_node(n)
     G.add_weighted_edges_from(edgeList)
     print("Loading NetworkX Data  - Finish")
     return G
 
 
-def generateNetwork(input,evalue=0):
-    expname = input.split(".")[0]
+def addSuperFamtoNetwork(G, file):
+    with open(file, "r") as f:
+        lines = f.readlines()
+    fam_superfam = {}
+    for line in lines:
+        fam, superfam = line.split("\t")
+        fam_superfam[fam] = superfam
+    for node in G.nodes():
+        # fallira per tutti quelli che non hanno un superfam
+        node_fam = node.strip().split("__")[0]
+        #print("NODE FAM", node_fam)
+        try:
+            G.nodes[node]['superfamily'] = fam_superfam[node_fam]
+        except KeyError:
+            #print(f'{node_fam} not in superfamlist')
+            pass
+    return G
+
+
+def generateNetwork(input, evalue=0, superfam_file_path=""):
+    #expname = input.split(".")[0]
     if path.isfile(input):
         df = loadData(input)
     elif path.isdir(input):
@@ -177,7 +204,7 @@ def generateNetwork(input,evalue=0):
         print("input not recognized")
         exit()
     if evalue != 0:
-        df = filterevalue(df,evalue)
+        df = filterevalue(df, evalue)
     df = removeSelfHit(df)
     if len(df) == 0:
         print("Lenght of the dataset after cleaning = 0 - I can't generate the gephi/cytoscape network")
@@ -188,17 +215,24 @@ def generateNetwork(input,evalue=0):
         print("Lenght of the dataset after cleaning = 0 - I can't generate the gephi/cytoscape network")
         pass
     else:
-        #df = mergeHits(df)
+        #df = mergeHits(df) can't be turned on after removing the duplicates
         df = evalToForce(df)
         df = normalize(df)
         G = networkxLoad(df)
+        if superfam_file_path:
+            G = addSuperFamtoNetwork(G, superfam_file_path)
         # write the GML file for Gephi
         of = path.abspath(input).split(".")[0]
         nx.write_gml(G,f"{of}.gml")
         # write json file for Cytoscape
-        fjson = open("{}.cytoscape".format(of),"w")
-        fjson.write(convert2cytoscapeJSON(G))
-        fjson.close()
+        #fjson = open("{}.cytoscape".format(of),"w")
+        #fjson.write(convert2cytoscapeJSON(G))
+        #fjson.close()
+
 
 if __name__ == "__main__":
-    generateNetwork(argv[1],float(argv[2]))
+    try:
+        superfam_file_path = argv[3]
+    except Exception:
+        superfam_file_path = ""
+    generateNetwork(argv[1], float(argv[2]), superfam_file_path)
